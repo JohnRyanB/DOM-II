75a78e8318ef1dfe931b55890d7287467cd63b3b
John-Blackburn <JRBbusinesscontact@gmail.com>
2021-06-06 19:34:07 -0400

    WIP

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+const signUpButton = document.getElementsByClassName("btn");
+console.log(signUpButton);
+
+const navButton = document.querySelectorAll(".nav-link");
+console.log(navButton);
